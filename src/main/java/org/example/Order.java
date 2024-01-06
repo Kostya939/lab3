929afc861a9e10f49633a38048faf816e1f54287
Kostya939 <91373707+Kostya939@users.noreply.github.com>
2024-01-06 14:39:06 +0200

    Update Order.java

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,22 +1,35 @@
 package org.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Order {
+    private static int nextOrderId = 1;
+
     private final int orderId;
-    private final String products;
+    private final List<Product> products;
     private final String status;
 
-    public Order (int orderId, String products, String status) {
+    public Order(int orderId, List<Product> products, String status) {
         this.orderId = orderId;
         this.products = products;
         this.status = status;
     }
 
-    public int getOrderId() {
-        return orderId;
+    public static int generateOrderId() {
+        return nextOrderId++;
     }
 
-    public String getProducts() {
-        return products;
+    @Override
+    public String toString() {
+        String productDetails = products.stream()
+                .map(Product::toString)
+                .collect(Collectors.joining("\n"));
+        return "Order ID: " + orderId + "\nProducts:\n" + productDetails + "\nStatus: " + status;
+    }
+
+    public int getOrderId() {
+        return orderId;
     }
 
     public String getStatus() {
