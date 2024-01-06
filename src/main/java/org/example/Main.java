41db97925fbb53d67cf12bb1569db19b83cc1863
Kostya939 <91373707+Kostya939@users.noreply.github.com>
2024-01-06 14:38:35 +0200

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("All works Great!");
+        Cart cart = new Cart();
+        Product product1 = new Product(1, "Product 1", 10.0);
+        cart.addProduct(product1);
+        cart.checkout();
     }
 }
